cc82bdd20631813be790878b433cb932379d826e
aleesha-wood <spectrat@spectratart.com>
2019-10-10 18:25:57 -0400

    added form fields and set up single user state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Here's something cool</h1>
+      <Form />
     </div>
   );
 }
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,1 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
+const Form = () => {
+  const [teamMember, setTeamMember] = useState({name:"", email:"", role:""});
+  const [name, setName] = useState("");
+  const[email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+
+  const handleNameChange = e => {
+    setTeamMember({...teamMember, name: e.target.value});
+  };
+  const handleEmailChange = e => {
+    setTeamMember({...teamMember, email: e.target.value});
+  };
+  const handleRoleChange = e => {
+    setTeamMember({...teamMember, role: e.target.value})
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(teamMember);
+  };
+
+  return (
+      <form onSubmit={e => handleSubmit(e)}>
+        <label>
+          Name:
+          <input 
+            type="text" 
+            name="name"
+            onChange={e => handleNameChange(e)} 
+          />
+        </label>
+
+        <label>
+          Email:
+          <input 
+            type="text"
+            name="email" 
+            onChange={e => handleEmailChange(e)} 
+          />
+        </label>
+
+        <label>
+          Role:
+          <select
+            type="select"
+            name="role"
+            onChange={e => handleRoleChange(e)}
+          >
+            <option value="frontend engineer">Frontend Engineer</option>
+            <option value="backend engineer">Backend Engineer</option>
+            <option value="UX designer">UX Designer</option>
+            <option value="iOS engineer">iOS Engineer</option>
+          </select>
+        </label>
+
+        <button>Submit!!</button>
+      </form>
+  )
+};
+
+export default Form;
