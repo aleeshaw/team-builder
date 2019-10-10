831b1e69ae1e36ad076505212e79a05095d8cda5
aleesha-wood <spectrat@spectratart.com>
2019-10-10 18:55:57 -0400

    code...DRY

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -2,24 +2,19 @@ import React, {useState} from 'react';
 
 
 const Form = () => {
-  const [teamMember, setTeamMember] = useState({name:"", email:"", role:""});
-  const [name, setName] = useState("");
-  const[email, setEmail] = useState("");
-  const [role, setRole] = useState("");
-
-  const handleNameChange = e => {
-    setTeamMember({...teamMember, name: e.target.value});
+  const [teamMember, setTeamMember] = useState({memberName:"", email:"", role:""});
+ 
+  const handleChange = e => {
+    setTeamMember({...teamMember, [e.target.name]: e.target.value});
   };
-  const handleEmailChange = e => {
-    setTeamMember({...teamMember, email: e.target.value});
-  };
-  const handleRoleChange = e => {
-    setTeamMember({...teamMember, role: e.target.value})
-  };
+  
 
   const handleSubmit = e => {
     e.preventDefault();
     console.log(teamMember);
+    console.log(teamMember.memberName);
+    console.log(teamMember.email);
+    console.log(teamMember.role);
   };
 
   return (
@@ -28,8 +23,8 @@ const Form = () => {
           Name:
           <input 
             type="text" 
-            name="name"
-            onChange={e => handleNameChange(e)} 
+            name="memberName"
+            onChange={e => handleChange(e)} 
           />
         </label>
 
@@ -38,7 +33,7 @@ const Form = () => {
           <input 
             type="text"
             name="email" 
-            onChange={e => handleEmailChange(e)} 
+            onChange={e => handleChange(e)} 
           />
         </label>
 
@@ -47,8 +42,9 @@ const Form = () => {
           <select
             type="select"
             name="role"
-            onChange={e => handleRoleChange(e)}
+            onChange={e => handleChange(e)}
           >
+            <option>Plese Select A Role</option>
             <option value="frontend engineer">Frontend Engineer</option>
             <option value="backend engineer">Backend Engineer</option>
             <option value="UX designer">UX Designer</option>
