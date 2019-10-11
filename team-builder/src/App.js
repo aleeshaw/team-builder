63edfd2957380f40d23a7205a54173ae05248f33
aleesha-wood <spectrat@spectratart.com>
2019-10-10 21:25:45 -0400

    styling and functionality

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form.js'
 
 function App() {
+  const [teamMembers, setMembers] = useState([
+    {
+      id: 1,
+      memberName: "test member",
+      email: "email@email.com",
+      role: "member role"
+    }
+  ]);
+
+  const addMember = teamMember => {
+    const newTeamMember = {
+      id: Date.now(),
+      memberName: teamMember.memberName,
+      email: teamMember.email,
+      role: teamMember.role,
+
+    };
+    setMembers([...teamMembers, newTeamMember]);
+    console.log("Members List: ",teamMembers)
+  };
+
   return (
     <div className="App">
-      <Form />
+      <Form addMemberFn={addMember} />
     </div>
   );
 }
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
+import './App.css';
 
-
-const Form = () => {
+const Form = props => {
   const [teamMember, setTeamMember] = useState({memberName:"", email:"", role:""});
  
   const handleChange = e => {
@@ -11,49 +11,58 @@ const Form = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(teamMember);
-    console.log(teamMember.memberName);
-    console.log(teamMember.email);
-    console.log(teamMember.role);
+    //setTeamMember({memberName:'', email:'', role:''});
+    props.addMemberFn(teamMember);
+    //console.log("Member Info", teamMember);
   };
 
   return (
-      <form onSubmit={e => handleSubmit(e)}>
-        <label>
-          Name:
-          <input 
-            type="text" 
-            name="memberName"
-            onChange={e => handleChange(e)} 
-          />
-        </label>
+    <div className="form-container">
+    <h2 className="title">Add a Team Member</h2>
 
-        <label>
-          Email:
-          <input 
-            type="text"
-            name="email" 
-            onChange={e => handleChange(e)} 
-          />
-        </label>
+    <form onSubmit={e => handleSubmit(e)}>
+      <label>
+        Name:
+        <input 
+          type="text" 
+          id="memberName"
+          name="memberName"
+          value={teamMember.memberName}
+          onChange={e => handleChange(e)} 
+        />
+      </label>
 
-        <label>
-          Role:
-          <select
-            type="select"
-            name="role"
-            onChange={e => handleChange(e)}
-          >
-            <option>Plese Select A Role</option>
-            <option value="frontend engineer">Frontend Engineer</option>
-            <option value="backend engineer">Backend Engineer</option>
-            <option value="UX designer">UX Designer</option>
-            <option value="iOS engineer">iOS Engineer</option>
-          </select>
-        </label>
+      <label>
+        Email:
+        <input 
+          type="text"
+          id="email"
+          name="email" 
+          value={teamMember.email}
+          onChange={e => handleChange(e)} 
+        />
+      </label>
 
-        <button>Submit!!</button>
-      </form>
+      <label>
+        Role:
+        <select
+          type="select"
+          id="role"
+          name="role"
+          value={teamMember.role}
+          onChange={e => handleChange(e)}
+        >
+          <option>Plese Select A Role</option>
+          <option value="frontend engineer">FrontendEngineer</option>
+          <option value="backend engineer">BackendEngineer</option>
+          <option value="UX designer">UX Designer</option>
+          <option value="iOS engineer">iOS Engineer</option>
+        </select>
+      </label>
+
+      <button className="sub-button">Add New Member!!</button>
+    </form>
+    </div>
   )
 };
 
